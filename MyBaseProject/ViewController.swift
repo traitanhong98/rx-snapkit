51f68a7f44cbc19fab8efb1a061d76c25d5ecc5c
ECO0542-HoangNM <hoangnguyen1@tdt.asia>
2022-06-15 15:02:10 +0700

    Initial Commit

---
+++ MyBaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBaseProject
+//
+//  Created by ECO0542-HoangNM on 15/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
